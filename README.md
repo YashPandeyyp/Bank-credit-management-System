fba20142037d2ae584d8275b2e765969ba9825a2
YashPandeyyp <69712801+YashPandeyyp@users.noreply.github.com>
2021-03-21 21:18:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Bank-credit-management-System
-The main aim of bank credit management system is to transfer credit to person as per user choice. Though  user can transfer credit to one person at a time by selecting name of that person. User can view list of person available in list by clicking on view user.  This project is one of the task given by sparks foundation.  
+# Credit-Management-WebApp
+Credit management web application created for the task given by Sparks Foundation Internship Program.
+
+
+The SQL queries for the database creation is present in the file compnay.sql
---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="Assets/css/style.css">
+</head>
+<body >
+<div id="header">
+       <br>
+       <h1 style=" font-family:Georgia; font-size: 60px  ; "> Manage Your Credit Efficiently with Yash</h1>
+       <br>
+       <h2 style=" font-family:Georgia; font-size: 50px  ; "> Credit management System </h2>
+        </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr>
+
+                <div class="css-button" >
+                  <p class="css-button-text">View Users</p>
+                  <div class="css-button-inner">
+                  <a href="getdetail.php" >
+                  <div class="reset-skew">
+                  <p class="css-button-inner-text">View Users</p>
+                </div></a>
+                </div>
+                </div>
+
+                </td>
+                </tr>
+
+                <tr>
+               <br> <br> <br> <br> <br>
+               <div class="css-button" >
+                 <p class="css-button-text">Credit Transfer</p>
+                 <div class="css-button-inner">
+                 <a href="selectuser.php" >
+                 <div class="reset-skew">
+                 <p class="css-button-inner-text">Credit Transfer</p>
+               </div></a>
+               </div>
+               </div>
+
+               </td>
+               </tr>
+
+            </table>
+    </div>
+
+</body>
+</html>
